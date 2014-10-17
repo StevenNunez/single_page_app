1baf83a3dfd5f732e40eb21795f5e9b2087024e1
Steven Nunez <steven.nunez@gmail.com>
2014-10-16 20:34:34 -0400

    Add Solution

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,33 @@
-$(function(){
+function fetchPage(page) {
+  var url = '/templates/' + page + '.html';
+  $.ajax(url).success(function(htmlText){
+    $('#content').html(htmlText);
+  });
+}
 
-  $.ajax('/templates/home.html').success(function(htmlText){
-    $('#content').html(htmlText)
+function init(){
+  $('.home-link, .about-link, .contact-link').click(function(e){
+    e.preventDefault();
+    var pageFromTarget = e.target.className.split('-')[0];
+    fetchPage(pageFromTarget);
   })
 
+  if (location.hash){
+    var page = location.hash.slice(1,location.hash.length);
+    fetchPage(page);
+  } else {
+    fetchPage('home');
+  }
+
+  $('#content').on('click', 'form.contact input[type=submit]', function(e){
+    e.preventDefault();
+    var data = $(this).parent('form').serialize();
+    $.post('/contact', data, function(response){
+      $('#content').html(response);
+    })
+  })
+}
+
+$(function(){
+  init();
 })
--- server.rb
+++ server.rb
@@ -2,4 +2,16 @@ class Server < Sinatra::Base
   get '/' do
     send_file File.join(settings.public_folder, 'index.html')
   end
+
+  get '/:template' do
+    if ['home', 'about', 'contact'].include?(params[:template])
+      redirect "/##{params[:template]}"
+    else
+      pass
+    end
+  end
+
+  post '/contact' do
+    "Thanks for your message"
+  end
 end
